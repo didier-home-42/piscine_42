6ee605144057019f6d9e2690747263c68ce6784e
defrfoyu <defrfoyu@gmail.com>
2026-09-05 22:43:18 +0200

    ex1 terminé

---
+++ 04-C-Programming-Fundamentals/ex1/ft_print_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manager <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/05 22:31:12 by manager           #+#    #+#             */
+/*   Updated: 2026/09/05 22:41:05 by manager          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	write(1, "abcdefghijklmopqrstuvwxyz\n", 27);
+}
+
+/* === Test === 
+
+char	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}*/
